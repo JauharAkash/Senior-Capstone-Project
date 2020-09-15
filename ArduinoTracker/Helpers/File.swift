cf6f2fd268a97a47c6ca3ab4d10b8f4b2630c989
Robert Piccirillo <robpic12@gmail.com>
2020-09-14 22:41:52 -0400

    Created Layout of Sign-In Screen

---
+++ ArduinoTracker/Helpers/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  ArduinoTracker
+//
+//  Created by Rob Piccirillo on 9/14/20.
+//  Copyright © 2020 Rob Piccirillo. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let MapViewController = "mapVC"
+    }
+}
--- ArduinoTracker/ViewControllers/RootViewController.swift
+++ ArduinoTracker/ViewControllers/RootViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RootViewController: UIViewController {
 
@@ -45,7 +46,41 @@ class RootViewController: UIViewController {
     @IBAction func switchViews(sender: UIBarButtonItem) {
         
     }
+    
+    // Check to make sure all fields were filled out correctly.
+//    func verifyFields() -> String? {
+//
+//        if emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+//
+//            return "Please fill in all fields."
+//        }
+//
+//    }
 
     @IBAction func loginPressed(_ sender: Any) {
+       
+        // TODO: Verify Login Information
+       //let error = verifyFields()
+        
+        // Create clean versions of the fields
+        let emailAddress = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Sign In User
+        Auth.auth().signIn(withEmail: emailAddress, password: password) { (result, error) in
+        
+        if error != nil  {
+            // Couldn't sign in
+            self.loginLabel.text = "No Password."
+        }
+        
+        else {
+            let MapViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.MapViewController) as? MapViewController
+            
+            self.view.window?.rootViewController = MapViewController
+            self.view.window?.makeKeyAndVisible()
+        }
+        
+        }
     }
 }
