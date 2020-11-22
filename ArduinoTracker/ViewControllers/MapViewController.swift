fb0f55b8e10bdec8628b95ac4addc05d5d5d84b6
Robert Piccirillo <robpic12@gmail.com>
2020-11-22 00:52:42 -0500

    Added read from userLocation collection, set coordinates to Hofstra

--- ArduinoTracker/ViewControllers/MapViewController.swift
+++ ArduinoTracker/ViewControllers/MapViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Firebase
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
 //class MapViewController: UIViewController {
@@ -58,18 +59,85 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
       //  manager.startUpdatingLocation()
         // Do any additional setup after loading the view.
     }
+    
+    func useCurrentLoc() -> Bool
+    {
+        return false
+    }
+    
     //Using current location of the device
     func locationManager(_ manager: CLLocationManager, didUpdateLocations
-        location: [CLLocation]){
-        guard let location = location.last else {return}
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: 200, longitudinalMeters: 200)
-        mapView.setRegion(region, animated: true)
-        
-        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        location: [CLLocation])
+    {
+        print(useCurrentLoc())
+        if (useCurrentLoc() == true)
+        {
+            
+            guard let location = location.last else {return}
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion.init(center: center, latitudinalMeters: 200, longitudinalMeters: 200)
+            mapView.setRegion(region, animated: true)
+        }
+        else
+        {
+            //readDBLocation()
+
+            let db = Firestore.firestore()
+            
+            let docRef = db.collection("userLocation").document("Hofstra")
+            
+            docRef.getDocument { (document, error) in
+                if let document = document, document.exists
+                {
+                    let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                    print("Document data: \(dataDescription)")
+                    let lat = document.get("latitude") as! Double
+                    let long = document.get("longitude") as! Double
+                    let coord = CLLocationCoordinate2DMake(lat, long)
+                    let region = MKCoordinateRegion.init(center: coord, latitudinalMeters: 200, longitudinalMeters: 200)
+                    self.mapView.setRegion(region, animated: true)
+                }
+                
+                else
+                {
+                    print("Document does not exist")
+                }
+            }
+
+
+        }
+        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus)
+        {
             //checkLocationAuthorization()
         }
     }
+    
+
+    /*func readDBLocation() -> CLLocationCoordinate2D
+    {
+        let db = Firestore.firestore()
+        
+        let docRef = db.collection("userLocation").document("Hofstra")
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists
+            {
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                print("Document data: \(dataDescription)")
+                let lat = document.get("latitude") as! Double
+                let long = document.get("longitude") as! Double
+                var coord = CLLocationCoordinate2DMake(lat, long)
+                return coord
+
+            }
+            
+            else
+            {
+                print("Document does not exist")
+            }
+        }
+
+    }*/
 /*
        // self.mapView
         let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: location[0].coordinate.latitude, longitude: location[0].coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)) // For zooming into the map
